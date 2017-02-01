e07f1ffb628b93535654a30f43def2daba2aaeec
Paulo Martinhago <pmartinhago@gmail.com>
2017-02-01 09:21:01 -0200

    projectname

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'PROJECT_NAME',
+    'name' => 'PROJECTNAME',
 
     /*
     |--------------------------------------------------------------------------
